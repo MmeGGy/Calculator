4933fc352e670b24934d8a0461e81335bebbadf4
MmeGGy <47536975+MmeGGy@users.noreply.github.com>
2022-11-08 14:36:05 +0200

    Add files via upload

---
+++ CalcUnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcUnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcUnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("799d345e-e76a-4569-b274-4c401bf62246")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcClassBr;
+using System.Data.SqlTypes;
+
+namespace CalcUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"C:\Users\User\Desktop\Автомати\Лаб 1\Calculator\CalcUnitTestProject1\XMLFile1.xml", "Operation", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            long expr_1 = Convert.ToInt64(TestContext.DataRow["expr_1"]);
+            long expr_2 = Convert.ToInt64(TestContext.DataRow["expr_2"]);
+            string exp = Convert.ToString(TestContext.DataRow["exp"]);
+
+            string result;
+            try
+            {
+                result = Convert.ToString(CalcClass.Sub(expr_1, expr_2));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                result = ex.ParamName;
+            }
+            Assert.AreEqual(exp, result);
+        }
+    }
+}
